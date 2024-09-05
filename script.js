52a909c01ce8f93a7431fa19b6c21d904b8d16d3
juditperea <22jperea@ibadia.cat>
2024-09-05 19:02:33 +0200

    Resizing of background, loaded first character sprite

--- script.js
+++ script.js
@@ -2,15 +2,40 @@ const canvas = document.getElementById('backgroundCanvas')
 const context = canvas.getContext('2d')
 const video = document.getElementById('backgroundVideo')
 
-video.play()
+// Sprite for 'Still Chihiro'
+const chihiro = new Image()
+chihiro.src = '/sprites/chihiro.png'
 
-function drawVideo() {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  context.drawImage(video, 0, 0, canvas.width, canvas.height)
-  requestAnimationFrame(drawVideo)
+// Initial sprite position
+let chihiroX
+let chihiroY
+
+function resize() {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+  chihiroX = canvas.width * 0.1
+  chihiroY = canvas.height * 0.75
 }
 
+window.addEventListener('resize', resize)
+
+resize()
 
 video.addEventListener('loadeddata', () => {
-  drawVideo()
+  video.play()
+  drawScene()
 })
+
+function drawScene() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+
+  // Draw background according to canvas size
+  context.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+  // Draw the sprite for still Chihiro (game is not playing)
+  context.drawImage(chihiro, chihiroX, chihiroY, canvas.width * 0.07, canvas.height * 0.18)
+
+  // Call the function to create loop effect
+  requestAnimationFrame(drawScene)
+}
